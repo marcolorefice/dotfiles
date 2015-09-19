f4f01564c9794c97de77bb7ed707eee956f1d425
Stefano Corallo <stefanorg@gmail.com>
2015-09-19 16:04:45 +0200

    cleanup

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -3,13 +3,17 @@ if [[ ! -d $HOME/zsh/lib/antigen ]];
 then
   git clone https://github.com/zsh-users/antigen.git $HOME/zsh/lib/antigen
 fi
+
 #loading antigen
 source $HOME/zsh/lib/antigen/antigen.zsh
 
-# antigen
+#antigen use oh-my-zsh
+
 antigen bundles <<EOBUNDLES
   zsh-users/zsh-syntax-highlighting
   zsh-users/zsh-completions src
 EOBUNDLES
+
+antigen theme pygmalion
 
 antigen apply
