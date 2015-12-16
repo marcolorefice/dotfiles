53afb6e6e559b6d575f7f1ab72b45e9abbea1a12
Stefano Corallo <stefanorg@gmail.com>
2015-12-16 21:43:34 +0100

    docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -9,3 +9,12 @@ DOCKER_MACHINE=$(which docker-machine)
 alias dms='$DOCKER_MACHINE status default'
 alias dmh='$DOCKER_MACHINE stop default'
 alias dml='$DOCKER_MACHINE ls'
+
+DOCKER_COMPOSE=$(which docker-compose)
+# #############
+#  COMPOSE 
+# #############
+alias dcu='$DOCKER_COMPOSE up -d'
+alias dcs='$DOCKER_COMPOSE stop'
+alias dcp='$DOCKER_COMPOSE ps'
+alias dcl='$DOCKER_COMPOSE logs'
